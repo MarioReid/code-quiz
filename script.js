6ce93e42b565ff6b51a98eeeefbe6cbb8b705619
Mario Reid <mario.reid83@gmail.com>
2020-12-14 23:37:53 -0500

    added var questions and answers

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 var timer = document.getElementById("timer");
-var startButton = document.getElementById("startButton")
-var mainContentEl = document.getElementById("mainContent")
+var startButton = document.getElementById("startButton");
+var mainContentEl = document.getElementById("mainContent");
 
+var questions = [
+  "Commonly used data types Do Not include:",
+  "The condition in an if/else statement is enclosed within ____.",
+  "Arrays in JavaScript can be used to store ____.",
+  "String values must be enclosed within ____ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:",];
 
-
+var answers = [
+  ["string", "booleans", "alerts", "numbers"],
+  ["quotes", "curly brackets", "parentheses", "square brackets"],
+  ["numbers and strings", "other arrays", "booleans", "all of the above"],
+  ["commas", "curly brackets", "quotes", "parentheses"],
+  ["JavaScript", "terminal/bash", "for loops", "console.log"],
+];
+var correctAnswers = ["alerts", "parentheses", "all of the above", "parentheses", "console.log"];
 
 //When hit start button: clear html, start timer, and ask 1st question
-
-
 
 //Set Timer
 var secondsLeft = 75;
 
 function setTime() {
-    setInterval(function() {
+  setInterval(function () {
     secondsLeft--;
-    timer.textContent = secondsLeft
+    timer.textContent = secondsLeft;
     // if(secondsLeft === 0) {
     //   clearInterval(timerInterval);
     //   sendMessage();
     // }
-
   }, 1000);
 }
 
-startButton.addEventListener("click", function() {
-   // Clear todoList element and update todoCountSpan
-    mainContentEl.innerHTML = "";
-    setTime();
-
+startButton.addEventListener("click", function () {
+  // Clear todoList element and update todoCountSpan
+  mainContentEl.innerHTML = "";
+  setTime();
 });
-
-
-
-
-
-
-
-
